8eb5855c716775ab0a849de65a72c34718cbaf99
Michael Christensen <MLC4447@hotmail.com>
2020-02-06 10:35:41 +0100

    Nedarvning

---
+++ Øvelse i Nedarvning mm/Øvelse i Nedarvning mm/Objekter2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Øvelse_i_Nedarvning_mm
+{
+    class Objekter2
+    {
+    }
+}
